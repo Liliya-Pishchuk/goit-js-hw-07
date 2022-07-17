0ba0187cc2f3643fbdfebf33e35e440e75d82529
Liliya-Pishchuk <metelyck@gmail.com>
2022-07-17 15:53:05 +0300

    hw-7 question-1 part-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+/* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div>; */
+const galleryContainer = document.querySelector(".gallery");
+const result = makeGallery(galleryItems);
+function makeGallery(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+galleryContainer.insertAdjacentHTML("beforeend", result);
